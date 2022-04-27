994bbbb4550b8de992744eea3c34ee383c912e73
Keshav Sompura <keshavsomp@gmail.com>
2022-04-27 15:14:44 -0700

    update visuals

--- sea_prediction.py
+++ sea_prediction.py
@@ -1,3 +1,4 @@
+from turtle import color
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -40,7 +41,11 @@ df = df_clean.set_index('year')
 
 
 # create scatter plot with year and sea level
+sns.set_theme(style = 'whitegrid')
 sns.scatterplot(data=df, x='year', y='CSIRO Adjusted Sea Level')
+plt.xlabel('Year')
+plt.ylabel('Sea Level (inches)')
+plt.title('Rise in Sea Level since 1880')
 plt.show()
 
 # use a linear regression to get slope and y-intercept and plot the line of best fit over the plot
@@ -54,4 +59,46 @@ Y = df['CSIRO Adjusted Sea Level'].to_numpy()
 
 # create linear regression model
 linreg = stats.linregress(X,Y)
-linreg
+
+
+# create plot with linear regresion line
+sns.scatterplot(data=df, x='year', y='CSIRO Adjusted Sea Level')
+plt.plot(X, X*linreg.slope + linreg.intercept, color='orange')
+plt.xlabel('Year')
+plt.ylabel('Sea Level (inches)')
+plt.title('Rise in Sea Level since 1880')
+plt.show()
+
+# extend regression line out to the year 2050
+future_year = np.arange(1880, 2051)
+sns.scatterplot(data=df, x='year', y='CSIRO Adjusted Sea Level')
+plt.plot(future_year, future_year*linreg.slope + linreg.intercept, color='orange')
+plt.xlabel('Year')
+plt.ylabel('Sea Level (inches)')
+plt.title('Rise in Sea Level since 1880')
+plt.show()
+
+# create a new regression model begining from the year 2000 to 2013 (most recent year) in order to see recent sea level changes
+X_2000 = np.extract(X >= 2000, X)
+X_2000
+
+# extract the last 14 element of Y to match to the years 2000 to 2013
+Y_2000 = Y[-14:]
+
+lin_2000 = stats.linregress(X_2000, Y_2000)
+
+recent_year = np.arange(2000,2051)
+
+# add new regression line in the year 2000
+
+
+sns.scatterplot(data=df, x='year', y='CSIRO Adjusted Sea Level')
+sns.lineplot(recent_year, recent_year*lin_2000.slope + lin_2000.intercept, color='red', label='Rise: 0.17 in/year')
+sns.lineplot(future_year, future_year*linreg.slope + linreg.intercept, color='orange', label='Rise: 0.06 in/year',linestyle='dashed')
+plt.legend()
+plt.xlabel('Year')
+plt.ylabel('Sea Level (inches)')
+plt.suptitle('Rise in Sea Level since 1880')
+plt.title('Data Source: US Environmental Protection Agency',loc='right',fontdict = {'fontsize' : 7})
+plt.show()
+
